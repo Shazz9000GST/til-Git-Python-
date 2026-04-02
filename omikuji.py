af694ed971e026177e15625cafb3d88ccb44053b
Shazz9000GST <155403421+Shazz9000GST@users.noreply.github.com>
2026-04-02 22:46:52 +0900

    Create omikuji.py

---
+++ omikuji.py
@@ -0,0 +1,6 @@
+import random
+kuji = ["大吉", "中吉", "小吉"]
+print(random.choice(kuji))
+# importでモジュールのrandomという機能を呼び出す
+# Rubyで言う配列みたいなものを作り、それをkujiへ代入
+# random.choiceで変数に定義された値の内、どれか一つをランダムに指定しprintで表示する
